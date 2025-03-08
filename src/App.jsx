30453f4c52d4e8eabadeaaf8e2e549d6e098ddc4
yasindu2000 <yasindudahanayake@gmail.com>
2025-03-08 21:04:31 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from './assets/components/Navbar'
+import Hero from './assets/components/Hero'
 
 function App() {
   return (
     <div>
 
       <Navbar/>
+      <Hero/>
     </div>
   )
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HeroImage from '../assets/logo.jpg'
+
+function Hero() {
+  return (
+    <div>
+        <img src={HeroImage} alt="" className="" />
+    </div>
+  )
+}
+
+export default Hero
