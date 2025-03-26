683505fde11a73a2a1328c3ee4e9af8ae903c86b
yasindu2000 <yasindudahanayake@gmail.com>
2025-03-26 16:08:20 +0530

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -28,7 +28,7 @@ function Hero() {
 <a
 href="/yasindu_cv.pdf" // Path to your CV in the public folder
 download="yasindu_cv.pdf" // Sets file download name
-className="bg-gradient-to-r from-pink-500 to-yellow-500 text-white hidden md:inline transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full cursor-pointer"
+className="bg-gradient-to-r from-pink-500 to-yellow-500 text-white md:inline transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full cursor-pointer"
 >
 Download CV
 </a>
