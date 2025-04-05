e94bf9ad0295109a503c1d1bf3f907c83b9d4173
yasindu2000 <yasindudahanayake@gmail.com>
2025-04-05 18:13:34 +0530

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -25,13 +25,13 @@ function Hero() {
         <div className='mt-12 space-x-4'>
           <button className="bg-gradient-to-r from-green-400 to-blue-500 text-white  md:inline transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full cursor-pointer  " ><Link to='contact' spy={true} smooth={true} duration={500}>Contact With Me</Link></button>
           {/* Download CV Button */}
-<a
+<button className="bg-gradient-to-r from-pink-500 to-yellow-500 text-white md:inline transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full cursor-pointer"><a
 href="/yasindu_cv.pdf" // Path to your CV in the public folder
 download="yasindu_cv.pdf" // Sets file download name
-className="bg-gradient-to-r from-pink-500 to-yellow-500 text-white md:inline transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full cursor-pointer"
+
 >
 Download CV
-</a>
+</a></button>
           
         </div>
         
